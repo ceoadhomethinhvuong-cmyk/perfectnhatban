4122ea17b62a9088f4829c7ddb9a380588017c1b
Phuong Tokyo <email_github_cua_chi@gmail.com>
2026-06-24 15:56:30 +0700

    add vendor for hosting

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -614,9 +614,9 @@ trợ chăm sóc da và sức khỏe từ bên trong.')
                                     <img data-lazyloaded="1"
                                         src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSI2ODAiIGhlaWdodD0iNDkwIiB2aWV3Qm94PSIwIDAgNjgwIDQ5MCI+PHJlY3Qgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgc3R5bGU9ImZpbGw6I2NmZDRkYjtmaWxsLW9wYWNpdHk6IDAuMTsiLz48L3N2Zz4="
                                         loading="lazy" decoding="async" width="680" height="490"
-                                        data-src="https://gsccos.vn/wp-content/uploads/2025/11/2_11zon.webp"
+                                        data-src="{{ asset('images/banner/banner2.webp') }}"
                                         class="attachment-full size-full wp-image-6217" alt="Chăm Từ Gốc"
-                                        data-srcset="https://gsccos.vn/wp-content/uploads/2025/11/2_11zon.webp 680w, https://gsccos.vn/wp-content/uploads/2025/11/2_11zon-300x216.webp 300w, https://gsccos.vn/wp-content/uploads/2025/11/2_11zon-600x432.webp 600w"
+                                        data-srcset="{{ asset('images/banner/banner2.webp') }} 680w,{{ asset('images/banner/banner2.webp') }} 300w, {{ asset('images/banner/banner2.webp') }} 600w"
                                         data-sizes="(max-width: 680px) 100vw, 680px" />
                                 </div>
                             </div>
